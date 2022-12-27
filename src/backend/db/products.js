0e4db16d3489014358eeb83bbff83dada3a9566d
Karthik Nayak <nayakkarthik466@gmail.com>
2022-12-27 09:40:59 +0530

    Initial commit

--- src/backend/db/products.js
+++ src/backend/db/products.js
@@ -1,4 +1,4 @@
-import { v4 as uuid } from "uuid";
+import { v4 as uuid } from 'uuid';
 
 /**
  * Product Database can be added here.
@@ -8,23 +8,240 @@ import { v4 as uuid } from "uuid";
 export const products = [
   {
     _id: uuid(),
-    title: "You Can WIN",
-    author: "Shiv Khera",
-    price: "5000",
-    categoryName: "non-fiction",
+    title: 'You Can WIN',
+    author: 'Shiv Khera',
+    price: '5000',
+    categoryName: 'non-fiction',
   },
   {
     _id: uuid(),
-    title: "You are Winner",
-    author: "Junaid Qureshi",
-    price: "3000",
-    categoryName: "horror",
+    title: 'You are Winner',
+    author: 'Junaid Qureshi',
+    price: '3000',
+    categoryName: 'horror',
   },
   {
     _id: uuid(),
-    title: "Think and Grow Rich",
-    author: "Shiv Khera",
-    price: "1000",
-    categoryName: "fiction",
+    title: 'Think and Grow Rich',
+    author: 'Shiv Khera',
+    price: '1000',
+    categoryName: 'fiction',
+  },
+];
+
+export const product = [
+  {
+    _id: uuid(),
+    title: 'earphone1',
+    price: '2000',
+    categoryName: 'Earphone',
+    rating: '',
+    image: 'https://i.postimg.cc/pTLPwsG3/earphones-a-1.webpp',
+  },
+  {
+    _id: uuid(),
+    title: 'earphone2',
+    price: '2500',
+    categoryName: 'Earphone',
+    rating: '',
+    image: 'https://i.postimg.cc/yxWpCRzV/earphones-a-2.webp',
+  },
+  {
+    _id: uuid(),
+    title: 'earphone3',
+    price: '800',
+    categoryName: 'Earphone',
+    rating: '',
+    image: 'https://i.postimg.cc/7Z4cff3N/earphones-a-3.webp',
+  },
+  {
+    _id: uuid(),
+    title: 'earphone4',
+    price: '1499',
+    categoryName: 'Earphone',
+    rating: '',
+    image: 'https://i.postimg.cc/Hn5ZLgjK/earphones-a-4.webp',
+  },
+  {
+    _id: uuid(),
+    title: 'earphone5',
+    price: '600',
+    categoryName: 'Earphone',
+    rating: '',
+    image: 'https://i.postimg.cc/FH0WvmwJ/earphones-b-1.webp',
+  },
+  {
+    _id: uuid(),
+    image: 'https://i.postimg.cc/wT84G7Gd/earphones-b-2.webp',
+  },
+  // {
+  //   _id: uuid(),
+  //   image: 'https://i.postimg.cc/RZxdXyqC/earphones-b-3.webp',
+  // },
+  // {
+  //   _id: uuid(),
+  //   image: 'https://i.postimg.cc/Bv6mKDX2/earphones-b-4.webp',
+  // },
+  // {
+  //   _id: uuid(),
+  //   image: 'https://i.postimg.cc/Fzjp4VBX/earphones-c-1.webp',
+  // },
+  // {
+  //   _id: uuid(),
+  //   image: 'https://i.postimg.cc/5NVpmDX4/earphones-c-2.webp',
+  // },
+  // {
+  //   _id: uuid(),
+  //   image: 'https://i.postimg.cc/FsNgSc61/earphones-c-3.webp',
+  // },
+  // {
+  //   _id: uuid(),
+  //   image: 'https://i.postimg.cc/g20VSpmW/earphones-c-4.webp',
+  //   name: 'c4',
+  // },
+  // {
+  //   _id: uuid(),
+  //   image:
+  //     'https://i.postimg.cc/vBYmQYBq/a64b345016e96adfb8849af5521c8e0ecfe8f027-555x555.webp',
+  // },
+  {
+    _id: uuid(),
+    title: 'headphone1',
+    price: '2000',
+    rating: '',
+    categoryName: 'Headphone',
+    image: 'https://i.postimg.cc/Y9QYGqf1/headphones-a-1.webp',
+  },
+  {
+    _id: uuid(),
+    title: 'headphone2',
+    price: '3000',
+    rating: '',
+    categoryName: 'Headphone',
+    image: 'https://i.postimg.cc/mDcR8t6H/headphones-a-2.webp',
+  },
+  {
+    _id: uuid(),
+    title: 'headphone3',
+    price: '5000',
+    rating: '',
+    categoryName: 'Headphone',
+    image: 'https://i.postimg.cc/fTQZfW7V/headphones-a-3.webp',
+  },
+  {
+    _id: uuid(),
+    title: 'headphone4',
+    price: '400',
+    rating: '',
+    categoryName: 'Headphone',
+    image: 'https://i.postimg.cc/yx4qjnZM/headphones-a-4.webp',
+  },
+  // {
+  //   _id: uuid(),
+  //   image: 'https://i.postimg.cc/yYK4SQB9/headphones-b-1.webp',
+  // },
+  // {
+  //   _id: uuid(),
+  //   image: 'https://i.postimg.cc/R0Njz2Qr/headphones-b-2.webp',
+  // },
+  // {
+  //   _id: uuid(),
+  //   image: 'https://i.postimg.cc/W3ZCdrHX/headphones-b-3.webp',
+  // },
+  // {
+  //   _id: uuid(),
+  //   image: 'https://i.postimg.cc/3J1603t5/headphones-b-4.webp',
+  // },
+  // {
+  //   _id: uuid(),
+  //   image: 'https://i.postimg.cc/wBybrLgg/headphones-c-1.webp',
+  // },
+  // {
+  //   _id: uuid(),
+  //   image: 'https://i.postimg.cc/xdwFV81H/headphones-c-2.webp',
+  // },
+  // {
+  //   _id: uuid(),
+  //   image: 'https://i.postimg.cc/66LbF9J2/headphones-c-3.webp',
+  // },
+  // {
+  //   _id: uuid(),
+  //   image: 'https://i.postimg.cc/0y6f5v3C/headphones-c-4.webp',
+  // },
+  {
+    _id: uuid(),
+    title: 'speaker1',
+    price: '4000',
+    rating: '',
+    categoryName: 'Speaker',
+    image: 'https://i.postimg.cc/4dGb1938/speaker1.webp',
+  },
+  // {
+  //   _id: uuid(),
+  //   image: 'https://i.postimg.cc/TY1jQGRX/speaker2.webp',
+  // },
+  // {
+  //   _id: uuid(),
+  //   image: 'https://i.postimg.cc/Xvf9XYg9/speaker3.webp',
+  // },
+  // {
+  //   _id: uuid(),
+  //   image: 'https://i.postimg.cc/rsPr7082/speaker4.webp',
+  // },
+  {
+    _id: uuid(),
+    title: 'speaker2',
+    price: '3000',
+    rating: '',
+    categoryName: 'Speaker',
+    image: 'https://i.postimg.cc/t4YjX1pm/61o-So-Hc7-Vt-L-AC-UY327-FMwebp-QL65.webp',
+  },
+  {
+    _id: uuid(),
+    title: 'speaker3',
+    price: '2999',
+    rating: '',
+    categoryName: 'Speaker',
+    image: 'https://i.postimg.cc/G37gbXtd/61-EXU8-Bu-GZL-AC-UY327-FMwebp-QL65.webp',
+  },
+  {
+    _id: uuid(),
+    title: 'speaker4',
+    price: '999',
+    rating: '',
+    categoryName: 'Speaker',
+    image: 'https://i.postimg.cc/C5X4k6n8/71-Kk-Kkx-NAb-L-AC-UY327-QL65.jpg',
+  },
+  {
+    _id: uuid(),
+    title: 'watch1',
+    price: '3999',
+    rating: '',
+    categoryName: 'Watch',
+    image: 'https://i.postimg.cc/SNn8CrrS/watch-1.webp',
+  },
+  {
+    _id: uuid(),
+    title: 'watch2',
+    price: '2000',
+    rating: '',
+    categoryName: 'Watch',
+    image: 'https://i.postimg.cc/PJ9C2g4t/watch-2.webp',
+  },
+  {
+    _id: uuid(),
+    title: 'watch3',
+    price: '600',
+    rating: '',
+    categoryName: 'Watch',
+    image: 'https://i.postimg.cc/Y2sLrFwJ/watch-3.webp',
+  },
+  {
+    _id: uuid(),
+    title: 'watch4',
+    price: '1500',
+    rating: '',
+    categoryName: 'Watch',
+    image: 'https://i.postimg.cc/kXR6B5JD/watch-4.webp',
   },
 ];
---
+++ src/pages/index.js
@@ -0,0 +1,3 @@
+import SignUp from './signuppage';
+
+export { SignUp };
